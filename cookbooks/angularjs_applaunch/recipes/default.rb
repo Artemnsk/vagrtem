a9129d1b7bf652140e01660c525492ac7a1b3be2
Artem Shelkov <shelkov1991@gmail.com>
2017-03-02 23:02:39 +0700

    change app run to not use subbranch.

--- cookbooks/angularjs_applaunch/recipes/default.rb
+++ cookbooks/angularjs_applaunch/recipes/default.rb
@@ -1,17 +1,15 @@
-app_name = ''
 apps_dir = "/var/www/"
-default_branch_dir = "trunk/"
 sites = {
     'shared' => {
-      'project_dir' => "#{app_name}_shared/",
+      'project_dir' => "shared/",
     },
     'app' => {
-      'app_url' => "#{app_name}_app.loc",
-      'project_dir' => "#{app_name}_app/",
+      'app_url' => "app.loc",
+      'project_dir' => "app/",
     },
     'portal' => {
-      'app_url' => "#{app_name}_portal.loc",
-      'project_dir' => "#{app_name}_portal/",
+      'app_url' => "portal.loc",
+      'project_dir' => "portal/",
     }
 }
 
@@ -48,27 +46,27 @@ bash 'Copy project if empty' do
   only_if { Dir.entries("#{apps_dir}/#{sites['app']['project_dir']}").size <= 2 }
 end
 # c. Set permissions for public directories. CSS.
-directory "#{apps_dir}/#{sites['app']['project_dir']}/#{default_branch_dir}/css/public" do
+directory "#{apps_dir}/#{sites['app']['project_dir']}/css/public" do
   owner 'vagrant'
   mode '0777'
   action :create
 end
 # d. Set permissions for public directories. JS.
-directory "#{apps_dir}/#{sites['app']['project_dir']}/#{default_branch_dir}/js/public" do
+directory "#{apps_dir}/#{sites['app']['project_dir']}/js/public" do
   owner 'vagrant'
   mode '0777'
   action :create
 end
 # e. Symbolic link to sharedservices project.
-link "#{apps_dir}/#{sites['app']['project_dir']}/#{default_branch_dir}/sharedservices" do
-  to "#{apps_dir}/#{sites['shared']['project_dir']}/#{default_branch_dir}"
+link "#{apps_dir}/#{sites['app']['project_dir']}/sharedservices" do
+  to "#{apps_dir}/#{sites['shared']['project_dir']}"
 end
 # f. npm install.
 bash 'NPM install for project' do
   code <<-EOH
       npm install
   EOH
-  cwd "#{apps_dir}/#{sites['app']['project_dir']}/#{default_branch_dir}"
+  cwd "#{apps_dir}/#{sites['app']['project_dir']}"
 end
 # g. conf file with virtualhost(-s).
 template "/etc/apache2/sites-available/#{sites['app']['app_url']}.conf" do
@@ -77,10 +75,10 @@ template "/etc/apache2/sites-available/#{sites['app']['app_url']}.conf" do
   group 'vagrant'
   mode '0755'
   variables({
-      "id" => "#{app_name}_app",
+      "id" => "app",
       "server_name" => sites['app']['app_url'],
       "apps_dir" => apps_dir,
-      "project_dir_absolute" => "#{apps_dir}/#{sites['app']['project_dir']}/#{default_branch_dir}",
+      "project_dir_absolute" => "#{apps_dir}/#{sites['app']['project_dir']}",
       "ssl_key" => node['certificate']['cert_key_path'],
       "ssl_cert" => node['certificate']['cert_cert_path'],
       "ssl_chain" => node['certificate']['cert_chain_path'],
@@ -96,6 +94,6 @@ bash 'Grunt' do
   code <<-EOH
       sudo grunt
   EOH
-  cwd "#{apps_dir}/#{sites['app']['project_dir']}/#{default_branch_dir}"
+  cwd "#{apps_dir}/#{sites['app']['project_dir']}"
 end
 ##########################################################################################
